f1a96ae1e2cabd9001dcac361db311bd981b5a27
mayha1 <dinhhoangnhatminh2002@gmail.com>
2023-01-27 00:02:23 +0700

    add board and pieces

---
+++ BoardnPieces.py
@@ -0,0 +1,149 @@
+import numpy as np
+
+board = np.array([[1, 1, 1, 1, 1, 1, 0],
+                  [1, 1, 1, 1, 1, 1, 0],
+                  [1, 1, 1, 1, 1, 1, 1],
+                  [1, 1, 1, 1, 1, 1, 1],
+                  [1, 1, 1, 1, 1, 1, 1],
+                  [1, 1, 1, 1, 1, 1, 1],
+                  [1, 1, 1, 0, 0, 0, 0]], np.int_)
+# OShape
+OShape = np.array([[1, 1, 1],
+                   [1, 1, 1]], np.int_)
+OShape = np.array([[1, 1],
+                   [1, 1],
+                   [1, 1]], np.int_)
+# TShape
+TShape = np.array([[1, 1, 1],
+                   [0, 1, 0]], np.int_)
+TShape = np.array([[0, 1, 0],
+                   [1, 1, 1]], np.int_)
+TShape = np.array([[1, 0],
+                   [1, 1],
+                   [1, 0]], np.int_)
+TShape = np.array([[0, 1],
+                   [1, 1],
+                   [0, 1]], np.int_)
+
+# UShape
+UShape = np.array([[1, 1, 1],
+                   [1, 0, 1]], np.int_)
+UShape = np.array([[1, 0, 1],
+                   [1, 1, 1]], np.int_)
+UShape = np.array([[1, 1],
+                   [0, 1],
+                   [1, 1]], np.int_)
+UShape = np.array([[1, 1],
+                   [1, 0],
+                   [1, 1]], np.int_)
+# GunShape
+GunShape = np.array([[1, 1, 1],
+                     [1, 1, 0]], np.int_)
+GunShape = np.array([[1, 1, 0],
+                     [1, 1, 1]], np.int_)
+GunShape = np.array([[1, 1, 1],
+                     [0, 1, 1]], np.int_)
+GunShape = np.array([[0, 1, 1],
+                     [1, 1, 1]], np.int_)
+GunShape = np.array([[1, 1],
+                     [1, 1],
+                     [1, 0]], np.int_)
+GunShape = np.array([[1, 1],
+                     [1, 1],
+                     [0, 1]], np.int_)
+GunShape = np.array([[1, 0],
+                     [1, 1],
+                     [1, 1]], np.int_)
+GunShape = np.array([[0, 1],
+                     [1, 1],
+                     [1, 1]], np.int_)
+# LShape
+LShape = np.array([[1, 1, 1, 1],
+                   [1, 0, 0, 0]], np.int_)
+LShape = np.array([[1, 1, 1, 1],
+                   [0, 0, 0, 1]], np.int_)
+LShape = np.array([[1, 0, 0, 0],
+                   [1, 1, 1, 1]], np.int_)
+LShape = np.array([[0, 0, 0, 1],
+                   [1, 1, 1, 1]], np.int_)
+LShape = np.array([[1, 1],
+                   [1, 0]
+                   [1, 0],
+                   [1, 0]], np.int_)
+LShape = np.array([[1, 0],
+                   [1, 0]
+                   [1, 0],
+                   [1, 1]], np.int_)
+LShape = np.array([[1, 1],
+                   [0, 1]
+                   [0, 1],
+                   [0, 1]], np.int_)
+LShape = np.array([[0, 1],
+                   [0, 1]
+                   [0, 1],
+                   [1, 1]], np.int_)
+# JShape
+JShape = np.array([[1, 1, 1, 1],
+                   [0, 1, 0, 0]], np.int_)
+JShape = np.array([[1, 1, 1, 1],
+                   [0, 1, 0, 0]], np.int_)
+JShape = np.array([[1, 1, 1, 1],
+                   [0, 1, 0, 0]], np.int_)
+JShape = np.array([[1, 1, 1, 1],
+                   [0, 1, 0, 0]], np.int_)
+
+JShape = np.array([[1, 0],
+                   [1, 1]
+                   [1, 0],
+                   [1, 0]], np.int_)
+JShape = np.array([[1, 0],
+                   [1, 0]
+                   [1, 1],
+                   [1, 0]], np.int_)
+JShape = np.array([[0, 1],
+                   [0, 1]
+                   [1, 1],
+                   [0, 1]], np.int_)
+JShape = np.array([[0, 1],
+                   [1, 1]
+                   [0, 1],
+                   [0, 1]], np.int_)
+# ZShape
+ZShape = np.array([[1, 1, 1, 0],
+                   [0, 0, 1, 1]], np.int_)
+ZShape = np.array([[0, 1, 1, 1],
+                   [1, 1, 0, 0]], np.int_)
+ZShape = np.array([[1, 1, 0, 0],
+                   [0, 1, 1, 1]], np.int_)
+ZShape = np.array([[0, 0, 1, 1],
+                   [1, 1, 1, 0]], np.int_)
+# LShape
+LShape = np.array([[1, 0],
+                   [1, 0]
+                   [1, 1],
+                   [0, 1]], np.int_)
+LShape = np.array([[0, 1],
+                   [1, 1]
+                   [1, 0],
+                   [1, 0]], np.int_)
+LShape = np.array([[0, 1],
+                   [0, 1]
+                   [1, 1],
+                   [1, 0]], np.int_)
+LShape = np.array([[1, 0],
+                   [1, 1]
+                   [0, 1],
+                   [0, 1]], np.int_)
+# VShape
+VShape = np.array([[1, 1, 1],
+                   [1, 0, 0],
+                   [1, 0, 0]])
+VShape = np.array([[1, 0, 0],
+                   [1, 0, 0],
+                   [1, 1, 1]])
+VShape = np.array([[1, 1, 1],
+                   [1, 0, 0],
+                   [1, 0, 0]])
+VShape = np.array([[1, 1, 1],
+                   [1, 0, 0],
+                   [1, 0, 0]])
